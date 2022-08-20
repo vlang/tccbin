af0a452eb5087517613ad90972bf465b7f538b8c
Delyan Angelov <delian66@gmail.com>
2022-08-20 08:10:44 +0000

    update binaries with the result of the buildtcc.sh script execution

--- lib/tcc/include/stddef.h
+++ lib/tcc/include/stddef.h
@@ -3,7 +3,7 @@
 
 typedef __SIZE_TYPE__ size_t;
 typedef __PTRDIFF_TYPE__ ssize_t;
-// typedef __WCHAR_TYPE__ wchar_t;
+typedef __WCHAR_TYPE__ wchar_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
 typedef __PTRDIFF_TYPE__ intptr_t;
 typedef __SIZE_TYPE__ uintptr_t;
