9f81f0a7a9620c0329d188f7889ced464ff995a4
Delyan Angelov <delian66@gmail.com>
2023-02-25 10:56:55 +0200

    update tcc to latest from git://repo.or.cz/tinycc.git - tcc version 0.9.27 mob:29ae3ed 2023-02-23T17:19:37-05:00 (AArch64 Darwin) , update the build.sh script too .

--- build.sh
+++ build.sh
@@ -10,20 +10,25 @@ pushd .
 git clone git://repo.or.cz/tinycc.git
 cd tinycc
 
-### NB: /v/v/tinycc/include is needed below,
-### to ensure proper support for bootstrapping tcc,
-### otherwise backtraces will be disabled
+### NB: the symlinks below are needed, to ensure proper support for bootstrapping tcc, otherwise backtraces will be disabled .
+for i in include/*.h; do echo $i; ln -s $i $(basename $i); done
+
 ./configure --prefix=thirdparty/tcc \
             --bindir=thirdparty/tcc \
             --crtprefix=thirdparty/tcc/lib:/usr/lib \
-            --libpaths=thirdparty/tcc/lib:/usr/lib:/lib:/usr/local/lib \
-            --sysincludepaths=thirdparty/tcc/lib/tcc/include:/v/v/tinycc/include:/usr/local/include:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
+            --libpaths=thirdparty/tcc/lib:/usr/local/lib:/usr/lib:/lib \
+            --sysincludepaths=thirdparty/tcc/lib/tcc/include:/usr/local/include:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
+	    --config-bcheck=yes --config-new_macho=yes --config-codesign --config-backtrace=yes \
             --debug
 make
 make install
 popd
 
 mv tinycc/thirdparty/tcc/* thirdparty/tcc/
+mv thirdparty/tcc/lib/tcc/*.o thirdparty/tcc/lib/
+mv thirdparty/tcc/lib/tcc/*.a thirdparty/tcc/lib/
+tree thirdparty/tcc/lib/
+
 mv thirdparty/tcc/tcc thirdparty/tcc/tcc.exe
 thirdparty/tcc/tcc.exe -v -v
 
