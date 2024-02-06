d34cfd4b8446834515ce186c71ea906c15ed05fb
Delyan Angelov <delian66@gmail.com>
2024-02-06 20:06:28 +0200

    update with `cp thirdparty/tcc/build.sh ./build.sh ; TCC_FOLDER=thirdparty/tcc TCC_COMMIT=105d70f ./build.sh`

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # thirdparty-freebsd-amd64
 
-This is a prebuild tcc (git://repo.or.cz/tinycc.git), cut at commit 20a1ebf .
+This is a prebuild tcc (git://repo.or.cz/tinycc.git), cut at commit 105d70f7 (2024-02-05) .
 
-It is compiled by copying the file buildtcc.sh into the V main repo
-folder, and doing: `./buildtcc.sh' on an AMD64/Intel based FreeBSD system.
-
+It is compiled by copying the file build.sh into the V main repo folder, and doing: 
+`TCC_FOLDER=thirdparty/tcc TCC_COMMIT=105d70f ./build.sh'
+on an AMD64/Intel based FreeBSD system.
---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/usr/local/bin/bash
+
+## error early:
+set -e
+
+## this script, should be run in V's main repo folder!
+
+export TCC_COMMIT="${TCC_COMMIT:-mob}"
+export TCC_FOLDER="${TCC_FOLDER:-thirdparty/tcc.$TCC_COMMIT}"
+
+echo "TCC_COMMIT: $TCC_COMMIT"
+echo "TCC_FOLDER: $TCC_FOLDER"
+echo ===============================================================
+
+rm -rf tinycc/
+rm -rf thirdparty/tcc.original/
+rsync -a thirdparty/tcc/ thirdparty/tcc.original/
+## rm -rf $TCC_FOLDER
+
+pushd .
+
+git clone git://repo.or.cz/tinycc.git
+cd tinycc
+
+git checkout $TCC_COMMIT
+
+export CC=cc
+export CFLAGS='-O3 -flto'
+
+./configure \
+            --prefix=$TCC_FOLDER \
+            --bindir=$TCC_FOLDER \
+            --crtprefix=$TCC_FOLDER/lib:/usr/lib:/usr/lib64 \
+            --sysincludepaths=$TCC_FOLDER/lib/tcc/include:/usr/local/include:/usr/include \
+            --libpaths=$TCC_FOLDER/lib/tcc:$TCC_FOLDER/lib:/usr/lib64:/usr/lib:/lib:/usr/local/lib \
+            --cc="$CC" \
+            --extra-cflags="$CFLAGS" \
+	    --extra-ldflags="-L/usr/local/lib -ltcc" \
+	    --config-backtrace=yes \
+	    --config-bcheck=yes \
+            --debug
+gmake
+gmake install
+
+popd
+
+rsync -a --delete tinycc/$TCC_FOLDER/                 $TCC_FOLDER/
+rsync -a          thirdparty/tcc.original/.git/       $TCC_FOLDER/.git/
+rsync -a          thirdparty/tcc.original/lib/libgc*  $TCC_FOLDER/lib/
+rsync -a          thirdparty/tcc.original/README.md   $TCC_FOLDER/README.md
+rsync -a          build.sh                            $TCC_FOLDER/build.sh
+mv                $TCC_FOLDER/tcc                     $TCC_FOLDER/tcc.exe
+
+$TCC_FOLDER/tcc.exe -v -v
+
+echo "tcc commit: $TCC_COMMIT . The tcc executable is ready in $TCC_FOLDER/tcc.exe "
