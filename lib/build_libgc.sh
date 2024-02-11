0a9106d532c025df2c7256b71122d6634a6db747
Delyan Angelov <delian66@gmail.com>
2024-02-11 09:42:30 +0200

    rebuild libgc.a from most recent https://github.com/ivmai/bdwgc f7e513ac06055310656e5c3c3bc9e1d84571e1b4, with new options. Update the build script, make it more easily runnable, to help future updates.

---
+++ lib/build_libgc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## this script should be run in V's main repo folder!
+
+set -e
+
+export CC="${CC:-gcc}"
+export TCC_FOLDER="${TCC_FOLDER:-thirdparty/tcc}"
+mkdir -p $TCC_FOLDER/lib/
+
+rm -rf bdwgc/
+git clone https://github.com/ivmai/bdwgc
+LIBGC_COMMIT=$(git -C bdwgc rev-parse HEAD)
+cd bdwgc/
+./autogen.sh
+CC=$CC CFLAGS='-Os -mtune=generic -fPIC' LDFLAGS='-Os -fPIC' ./configure \
+	--enable-handle-fork=yes \
+	--enable-sigrt-signals \
+	--enable-rwlock \
+	--enable-threads=pthreads \
+	--enable-static \
+	--enable-shared=no \
+	--enable-parallel-mark \
+	--enable-single-obj-compilation \
+	--enable-gc-debug
+
+make
+cd ..
+
+cp bdwgc/.libs/libgc.a $TCC_FOLDER/lib/libgc.a
+echo $LIBGC_COMMIT > $TCC_FOLDER/lib/libgc_commit.txt
+
+ls -la $TCC_FOLDER/lib/libgc.a
+echo "done compiling libgc, at commit $LIBGC_COMMIT ."
