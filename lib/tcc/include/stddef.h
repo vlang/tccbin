5c474f18d0c392c1676af4e6c430f1b7c36d4e8c
Delyan Angelov <delian66@gmail.com>
2024-09-24 12:40:52 +0300

    Revert "add preliminary support for linux-riscv64 (#53)" (it should have been on the thirdparty-linux-riscv64 branch, not master)
    This reverts commit 4b0f47859324c6f8e28b92256fdc7ffb95304b79.

--- lib/tcc/include/stddef.h
+++ lib/tcc/include/stddef.h
@@ -12,16 +12,33 @@ typedef __SIZE_TYPE__ uintptr_t;
 typedef union { long long __ll; long double __ld; } max_align_t;
 #endif
 
+#ifndef __int8_t_defined
+#define __int8_t_defined
+typedef signed char int8_t;
+typedef signed short int int16_t;
+typedef signed int int32_t;
+#ifdef __LP64__
+typedef signed long int int64_t;
+#else
+typedef signed long long int int64_t;
+#endif
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+#ifdef __LP64__
+typedef unsigned long int uint64_t;
+#else
+typedef unsigned long long int uint64_t;
+#endif
+#endif
+
 #ifndef NULL
 #define NULL ((void*)0)
 #endif
 
-#undef offsetof
 #define offsetof(type, field) ((size_t)&((type *)0)->field)
 
-#if defined __i386__ || defined __x86_64__
 void *alloca(size_t size);
-#endif
 
 #endif
 
